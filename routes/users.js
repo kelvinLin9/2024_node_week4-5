18a9d72e5cd022160024334a50fb02f4a6052fc3
kelvin <kelvin80121@gmail.com>
2024-05-16 22:19:33 +0800

    chore: Add update password functionality and endpoint for updating user password

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ import {
   login, 
   signup,
   forgetPassword,
+  updatePassword,
   getInfo,
   updateInfo,
 } from '../controllers/users.js';
@@ -15,6 +16,7 @@ router.use(checkRequestBodyValidator);
 router.post('/login', login);
 router.post('/signup', signup);
 router.post('/forgetPassword', forgetPassword);
+router.put('/updatePassword',  updatePassword);
 router.get('/profile', isAuth, getInfo);
 router.put('/profile', isAuth, updateInfo);
 
